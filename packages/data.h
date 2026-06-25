2abae97d8ce89dc0f6639be16578f52383e8f88a
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-06-25 15:12:04 -0300

    defini as funções de persistencia

--- packages/data.h
+++ packages/data.h
@@ -1,6 +1,10 @@
 #ifndef DATAH
 #define DATAH
-void salvarCSV();
+
+#include "stations.h"
+
+void salvarCSV(Estacao *lista, int qtdEstacoes); //persiste os dados em um arquivo com base em uma lista de estações e a quantidade de estações desta
+void carregarCSV(Estacao *lista, int qtdEstacoes, const char *nomeArquivo);// carrega um arquivo cujo nome deve ser informado, extraindo as informações pra memória do programa
 
 
 
