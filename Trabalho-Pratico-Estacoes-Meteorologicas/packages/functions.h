7c82d1b4e12758fa24d59b2dc19cdc368d9f47d3
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-06-26 00:39:49 -0300

    Algumas correções. Ainda sofrendo com as leituras

--- Trabalho-Pratico-Estacoes-Meteorologicas/packages/functions.h
+++ Trabalho-Pratico-Estacoes-Meteorologicas/packages/functions.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTIONSH
 #define FUNCTIONSH
+#include "../packages/stations.h"
 int verificaCorteEntrada(char *str);//Verifica se a entrada foi cortada pelo limite do buffer na leitura de dados. Retorna 1 caso haja erro, 0 caso esteja tudo certo.
 int verificaAlfa(char *str);//Verifica que a string só contém caracteres alfabéticos. Retorna 1 caso haja caracteres estranhos à regra, 0 caso sejam todos alfabéticos.
 int verificaData(DataLeitura data);//Verifica a validade da data inserida. Retorna 1 caso haja alguma incoerencia, 0 caso esteja tudo de acordo.
--- Trabalho-Pratico-Estacoes-Meteorologicas/source/stations.c
+++ Trabalho-Pratico-Estacoes-Meteorologicas/source/stations.c
@@ -9,9 +9,11 @@
 void adicionarEstacao(Estacao *name){
     char temp;
     printf("Registrando nova estação...\nInsira o ID da Estação:\n");
-    while(scanf("%d",name->id)<0||name->id>9999){
-    printf("ID inválido. O identificador deve ser tal que 0<ID<9999.\n");
+    while(scanf("%d%c",&name->id,&temp)!=2||name->id>9999||name->id<0){
+    printf("ID inválido. O identificador deve ser tal que 0<ID<9999.\n");  
+    while(getchar() != '\n'); //limpador de buffer
     }
+  
     printf("ID registrado. Informe o nome da estação.\n");
     fgets(name->nome,sizeof(name->nome),stdin);
     while(verificaCorteEntrada(name->nome)||verificaAlfa(name->nome)){
@@ -29,17 +31,19 @@ void adicionarEstacao(Estacao *name){
         printf("Sensor inválido. Tenha certeza de que o nome tenha no máximo 59 caracteres,todos alfabéticos,e tente novamente.\n");
         fgets(name->sensor,sizeof(name->sensor),stdin);
     } printf("Sensor registrado. Estação pronta para registrar leituras! Informe o número de leituras a serem feitas.\n");
-    while(scanf("%d%c",&name->n,&temp)!=2||temp!='\n'||name->n<=0||name->n>9999){ printf("Número inválido. Certifique-se de inserir somente um inteiro entre 1 e 9999");
-        scanf("%d%c",&name->n,&temp);
-    } printf("Registrando %d leituras. Digite-as uma por vez.",name->n);
+    while(scanf("%d%c",&name->n,&temp)!=2||temp!='\n'||name->n<=0||name->n>9999){ printf("Número inválido. Certifique-se de inserir somente um inteiro entre 1 e 9999\n");
+    while (getchar() != '\n'); //limpador de buffer
+    } printf("Registrando %d leituras. Digite-as uma por vez.\n",name->n);
     for (int i =0;i<name->n;i++){
-        if(scanf("%f%c",&name->leituras[i])!=2||temp!='\n'){
+        if(scanf("%f%c",&name->leituras[i],&temp)!=2||temp!='\n'){
             printf("Leitura inválida. Tente novamente.\n");
             i--;
+            while (getchar() != '\n');// limpador de buffer
         }
     }   printf("Leituras registradas. Informe a data da leitura (dd/mm/aaaa).\n");
     while(scanf("%d/%d/%d",&name->data.dia,&name->data.mes,&name->data.ano)!=3||verificaData(name->data)){
         printf("Data inválida. Certifique-se de inserir uma data válida seguindo o modelo dd/mm/aaaa.\n");
+        while (getchar() != '\n');// limpador de buffer
     } printf("Data registrada com sucesso. Calculando estatísticas...\n");
     name->media=calcularMediaRecursiva(name->leituras,name->n);
     name->variancia= calcularVariancia(name->leituras,name->n);
